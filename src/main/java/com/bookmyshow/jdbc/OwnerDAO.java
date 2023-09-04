ad7689666117e9a339adc2f58f61ffd16d4055bc
Sai Manoj Kumar Jakkani <48580137+SaiManojKumarJakkani@users.noreply.github.com>
2023-09-04 21:10:18 +0530

    Add files via upload

---
+++ src/main/java/com/bookmyshow/jdbc/OwnerDAO.java
@@ -0,0 +1,55 @@
+package src.main.java.com.bookmyshow.jdbc;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import src.main.java.com.bookmyshow.model.*;
+
+public class OwnerDAO {
+        private Connection connection;
+        private String tableName="Owner";
+        public OwnerDAO(){
+            connection=DatabaseConnectionManager.getConnection();
+        }
+
+        public List<User> getAllOwners(){
+            String sqlQuery = "SELECT * FROM " + tableName;
+            List<User> owners=new ArrayList<User>();
+            try{
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery(sqlQuery);
+            int columnCount = rs.getMetaData().getColumnCount();
+
+            while (rs.next()) {
+                User u=new Owner(rs.getString("userID"), rs.getString("password"),rs.getString("firstName"),rs.getString("lastName"),rs.getString("emailID"),rs.getString("bankAccNo") );
+                owners.add(u);
+            }
+            }
+            catch(Exception e){
+                System.out.println(e);
+                
+            }
+            return owners;
+        };
+        
+        public User getOwner(String userID){
+            String sqlQuery = "SELECT * FROM " + tableName+ " WHERE userID = '"+userID+"'";
+            User owner=null;
+            try{
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery(sqlQuery);
+            rs.next();
+            owner=new Owner(rs.getString("userID"), rs.getString("password"),rs.getString("firstName"),rs.getString("lastName"),rs.getString("emailID"),rs.getString("bankAccNo") );
+            
+        }
+            catch(Exception e){
+                System.out.println(e);
+            }
+            return owner;
+        };
+        
+        
+    
+}
