3094c27d45a6256ea5cdf069c047196d472cd755
prajaktaRSA <57608327+prajaktaRSA@users.noreply.github.com>
2023-09-08 11:02:14 +0530

    Add files via upload

--- src/main/java/com/bookmyshow/jdbc/BookingDAO.java
+++ src/main/java/com/bookmyshow/jdbc/BookingDAO.java
@@ -1,5 +1,38 @@
-package src.main.java.com.bookmyshow.jdbc;
+package org.example;
 
 public class BookingDAO {
-    
+
+    public static void getAllBookingDetails() throws Exception {
+        String tableName = "Booking";
+        String sqlQuery = "SELECT * FROM " + tableName;
+        JDBCMain.getDataJDBC(sqlQuery, tableName);
+    }
+    public  static void getVenueID() throws Exception {
+        //get venueID from venue table
+        String tableName = "Booking";
+        String sqlQuery = "SELECT venueid FROM " + tableName;
+        JDBCMain.getDataJDBC(sqlQuery,tableName);
+    }
+
+    public  static void getCustID() throws Exception{
+        //get custID from customer table
+        String tableName = "Booking";
+        String sqlQuery = "SELECT userID FROM " + tableName;
+        JDBCMain.getDataJDBC(sqlQuery,tableName);
+    }
+
+    public static void getShowID() throws Exception {
+        String tableName = "Booking";
+        String sqlQuery = "SELECT showID FROM " + tableName;
+        JDBCMain.getDataJDBC(sqlQuery,tableName);
+        //get showID from show table
+    }
+    public static void getSevenDaysBookingHistory()  throws Exception {
+        String tableName = "Booking";
+        String sqlQuery = "SELECT * FROM " + tableName + " LIMIT 7";
+        JDBCMain.getDataJDBC(sqlQuery,tableName);
+
+    }
+
+
 }
