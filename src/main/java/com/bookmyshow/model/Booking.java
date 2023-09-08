d0b712ca4c06ac7f99dc3563fcc2b39e3aab790c
prajaktaRSA <57608327+prajaktaRSA@users.noreply.github.com>
2023-09-08 10:59:18 +0530

    Add files via upload

--- src/main/java/com/bookmyshow/model/Booking.java
+++ src/main/java/com/bookmyshow/model/Booking.java
@@ -1,5 +1,127 @@
-package src.main.java.com.bookmyshow.model;
-
+package org.example;
+import java.util.ArrayList;
+import java.util.List;
 public class Booking {
-    
+    private  String bookingID;//Primary key
+    private  String custID;//foreign key
+
+    private  String venueID;//foreign key
+    private  String showID;//foreign key
+    private  String bookingDate; //read from user
+    private  int numSeatsBooked;//read from user
+    private  String pricingTierChosen; //read from user
+    private  String bankDetailsCust;//bank account number and IFSC code-2d array for each customer
+    private  String transactionStatus;//hardcode in json
+
+    public Booking() {
+        this.bookingID="";
+        this.custID="";
+        this.venueID="";//take from venue class
+        this.showID="";//take from show class
+        this.bookingDate="";
+        this.numSeatsBooked=0;//read from user
+        this.bankDetailsCust=""; // when to take arraylist vs string list
+        this.pricingTierChosen="";// choose from [gold, silver, platinum]
+        this.transactionStatus="";//Failed or successful
+    }
+    public Booking(String bookingID, String custID,  String venueID, String showID,
+                   String bookingDate, int numSeatsBooked, String bankDetailsCust,
+                   String pricingTierChosen, String transactionStatus) {
+        this.bookingID=bookingID;
+        this.custID= custID;
+        this.venueID=venueID;
+        this.showID=showID;
+        this.bookingDate=bookingDate;
+        this.numSeatsBooked=numSeatsBooked;
+        this.bankDetailsCust=bankDetailsCust;
+        this.pricingTierChosen=pricingTierChosen;
+        this.transactionStatus=transactionStatus;//depends on the payment gateway.
+    }
+
+    //Getter and setter functions
+    public String getBookingID() {
+        return bookingID;
+    }
+    public void setBookingID(String bookingID) {
+        this.bookingID = bookingID;
+    }
+
+    //Getter and setter functions
+    public String getCustID() {
+        return custID;
+    }
+    public void setCustID(String custID) {
+        this.custID = custID;
+    }
+
+    //Getter and setter functions
+    public String getVenueID() {
+        return venueID;
+    }
+    public void setVenueID(String venueID) {
+        this.venueID = venueID;
+    }
+
+    //Getter and setter functions
+    public String getShowID() {
+        return showID;
+    }
+    public void setShowID(String showID) {
+        this.showID = showID;
+    }
+
+    //Getter and setter functions
+    public String getBookingDate() {
+        return bookingDate;
+    }
+    public void setBookingDate(String bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    //Getter and setter functions
+    public int getNumSeatsBooked() {
+        return numSeatsBooked;
+    }
+    public void setNumSeatsBooked(int numSeatsBooked) {
+        this.numSeatsBooked = numSeatsBooked;
+    }
+
+    //Getter and setter functions
+    public String getBankDetailsCust() {
+        return bankDetailsCust;
+    }
+    public void setBankDetailsCust(String bankDetailsCust) {
+        this.bankDetailsCust = bankDetailsCust;
+    }
+
+    //Getter and setter functions
+    public String getPricingTierChosen() {
+        return pricingTierChosen;
+    }
+    public void setPricingTierChosen(String pricingTierChosen) {
+        this.pricingTierChosen = pricingTierChosen;
+    }
+
+    //Getter and setter functions
+    public String getTransactionStatus() {
+        return transactionStatus;
+    }
+    public void setTransactionStatus(String transactionStatus) {
+        this.transactionStatus = transactionStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "bookingID: "+bookingID+
+                "\n custID: "+custID+
+                "\n venueID: "+venueID+
+                "\n showID: "+showID+
+                "\n bookingDate: "+bookingDate+
+                "\n numSeatsBooked: "+numSeatsBooked+
+                "\n bankDetailsCust: "+bankDetailsCust+
+                "\n pricingTierChosen: "+pricingTierChosen+
+                "\n transactionStatus: "+transactionStatus;
+    }
+
+
 }
---
+++ src/main/java/com/bookmyshow/model/Movies.java
@@ -0,0 +1,122 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movies {
+    private String movieID;
+    private String movieName;
+    private String genre;
+    private String actor;
+    private String actress;
+    private String producer;
+    private String director;
+    private String language;
+    private String rating;
+
+    public Movies() {
+        this.movieID= "";
+        this.movieName="";
+        this.genre="";
+        this.actor="";
+        this.actress="";
+        this.producer="";
+        this.director="";
+        this.language="";
+        this.rating="";
+
+    }
+
+    public Movies(String movieID, String movieName, String genre, String actor, String actress, String producer, String director, String language, String rating) {
+    this.movieID=movieID;
+    this.movieName=movieName;
+    this.genre=genre;
+    this.actor=actor;
+    this.actress= actress;
+    this.producer=producer;
+    this.director=director;
+    this.language=language;
+    this.rating=rating;
+    }
+
+    //Getter Setter for movieID
+    public String getMovieID() {
+        return movieID;
+    }
+
+    public void setMovieID(String movieID) {
+        this.movieID = movieID;
+    }
+
+    //Getter Setter for movie Name
+    public String getMovieName() {
+        return movieName;
+    }
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    //Getter Setter for genre
+    public String getGenre() {
+        return genre;
+    }
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    //Getter Setter for actor
+    public String getActor() {
+        return actor;
+    }
+    public void setActor(String actor) {
+        this.actor = actor;
+    }
+    //Getter Setter for actress
+    public String getActress() {
+        return actress;
+    }
+    public void setActress(String actress) {
+        this.actress = actress;
+    }
+    //Getter Setter for producer
+    public String getProducer() {
+        return producer;
+    }
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+    //Getter Setter for director
+    public String getDirector() {
+        return director;
+    }
+    public void setDirector(String director) {
+        this.director= director;
+    }
+    //Getter Setter for language
+    public String getLanguage() {
+        return language;
+    }
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+    //Getter Setter for rating
+    public String getRating() {
+        return rating;
+    }
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+    @Override
+    public String toString() {
+        return "MovieID: "+movieID+
+                "\n movieName: "+movieName+
+                "\n genre: "+genre+
+                "\n actor: " +actor+
+                "\n actress: " +actress+
+                "\n producer: " + producer +
+                "\n director: " + director +
+                "\n language: "+ language +
+                "\n rating: " +rating;
+
+
+    }
+}
