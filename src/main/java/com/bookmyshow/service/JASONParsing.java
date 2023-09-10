8586f4f0134961fd24e6e5912cab1ba26a4cac3c
Addrija <46642423+Addrija@users.noreply.github.com>
2023-09-11 00:48:50 +0530

    Update JASONParsing.java

--- src/main/java/com/bookmyshow/service/JASONParsing.java
+++ src/main/java/com/bookmyshow/service/JASONParsing.java
@@ -20,5 +20,42 @@ public class JASONParsing {
         }
         return user;
     }
+    public static Customer parseDataCustomer(String filePath){
+         Customer user=null;
+        try{
+            ObjectMapper om=new ObjectMapper();
+            File jsonfile=new File(filePath);
+            user=om.readValue(jsonfile,Customer.class);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        return user;
+    }
     
+    public static Booking parseBookingData(String filepath) {
+        Booking booking = null;
+        ObjectMapper objectmap = new ObjectMapper();//Create object of ObjectMapper
+        try {
+            File jsonFile = new File(filepath);//Create object of jsonfile
+            booking= objectmap.readValue(jsonFile,Booking.class);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return booking;
+    }
+
+    public static ShowDetails parseDataShowDetails(String filePath){
+        ShowDetails showDetails =null;
+        try{
+            ObjectMapper om=new ObjectMapper();
+            File jsonfile=new File(filePath);
+            showDetails=om.readValue(jsonfile,ShowDetails.class);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        return showDetails;
+    }
 }
