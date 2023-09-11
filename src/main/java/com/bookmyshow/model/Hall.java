cb7ea7053b20462e0a2d0c6661a93da7a116e24b
Yashi Sahu <88144946+sahu-yashi@users.noreply.github.com>
2023-09-11 11:33:20 +0530

    Add files via upload

--- src/main/java/com/bookmyshow/model/Hall.java
+++ src/main/java/com/bookmyshow/model/Hall.java
@@ -3,6 +3,7 @@ package src.main.java.com.bookmyshow.model;
 public class Hall {
     private int hallId;
     private int venueId;
+    private String hallName;
     private int numSeatsGold;
     private int numSeatsSilver;
     private int numSeatsPlatinum;
@@ -11,6 +12,7 @@ public class Hall {
         this.numSeatsGold = 0;
         this.numSeatsSilver = 0;
         this.numSeatsPlatinum = 0;
+        this.hallName="";
     }
     public int getHallId() {
         return hallId;
@@ -26,6 +28,14 @@ public class Hall {
     public void setVenueId(int venueId) {
         this.venueId = venueId;
     }
+
+    public String getHallName() {
+        return hallName;
+    }
+    public void setHallName(String hallName) {
+        this.hallName = hallName;
+    }
+
     public int getNumSeatsGold() {
         return numSeatsGold;
     }
